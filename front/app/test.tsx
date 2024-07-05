196e9b281bdcb50d2615c041c0d6ceb5f8d47af3
渡邉友樹 <>
2024-07-05 10:19:48 +0900

    test: フォント設定確認用

---
+++ front/app/test.tsx
@@ -0,0 +1,12 @@
+const test = () => {
+    return (
+        <div className="font-body">
+            Enter
+            <div className="font-accent">
+                Enter
+            </div>
+        </div>
+    );
+}
+
+export default test;
