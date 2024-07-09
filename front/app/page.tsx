2202de1f0a17a7a5d8ea85e5ba7462762ea98895
渡邉友樹 <>
2024-07-09 20:05:51 +0900

    fix: Service クラス呼び出し例

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import PostService from "@/services/PostService";
 
 export default function Home() {
+  const postService = new PostService()
+  postService.getList().then((data) => console.log(JSON.stringify(data)))
   return (
     <div></div>
   )
