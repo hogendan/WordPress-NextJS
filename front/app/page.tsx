cdf187f4b7fe76451872df8e200a8fc034b00004
渡邉友樹 <>
2024-07-09 20:39:18 +0900

    fix: console に出す json フォーマットを見やすくインデント

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -3,7 +3,7 @@ import PostService from "@/services/PostService";
 
 export default function Home() {
   const postService = PostService
-  postService.getList().then((data) => console.log(JSON.stringify(data)))
+  postService.getList().then((data) => console.log(JSON.stringify(data, null, 2)))
   return (
     <div></div>
   )
