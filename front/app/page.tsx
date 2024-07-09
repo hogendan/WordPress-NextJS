73813f0edc0274b881c5fa848ee40e71e47a18cb
渡邉友樹 <>
2024-07-09 20:21:41 +0900

    refactor: インスタンス化やめたので呼び出し側も修正

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import PostService from "@/services/PostService";
 
 export default function Home() {
-  const postService = new PostService()
+  const postService = PostService
   postService.getList().then((data) => console.log(JSON.stringify(data)))
   return (
     <div></div>
