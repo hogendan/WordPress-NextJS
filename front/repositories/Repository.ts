5952a860c0fde01d9eeb1b939afae637d2c17699
渡邉友樹 <>
2024-07-10 16:50:38 +0900

    refactor: ローカル定数を .env.local ファイルに設定して、そこから読み取る。.env.local は git 管理しないよう設定。

--- front/repositories/Repository.ts
+++ front/repositories/Repository.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const repository = axios.create({
-    baseURL: 'http://localhost:8000/graphql',
+    baseURL: process.env.WP_ENDPOINT,
     headers: { "Content-Type": "application/json" }
 })
 
