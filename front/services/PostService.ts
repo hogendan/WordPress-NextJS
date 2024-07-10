0e70f9fbfa85d7bcbeb902e9baeb079eda9e5dce
渡邉友樹 <>
2024-07-10 09:16:15 +0900

    refactor: 投稿の型付をする

--- front/services/PostService.ts
+++ front/services/PostService.ts
@@ -1,7 +1,8 @@
+import PostType from "@/PostType";
 import RepositoryFactory from "@/repositories/RepositoryFactory";
 
 class PostService {
-    static async getList() {
+    static async getList(): Promise<PostType[]> {
         try {
             const res = await RepositoryFactory.post.getList();
             return res.data.data.posts.edges.map((data: any) => {
